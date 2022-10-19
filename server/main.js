f63c502b9456f82ca668283212beea2998f00649
효승 <hyoseung@hyoseung-ui-MacBook-Pro.local>
2022-10-19 18:27:40 +0900

    deploying with Galaxy

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { ContactCollection } from "../imports/api/ContactCollection";
+//api 넣어줌 => 서버에 적용해야함, 안그러면 실행안됌
+//DB에 저장완료
 
 Meteor.startup(() => {
   // If the Links collection is empty, add some data.
